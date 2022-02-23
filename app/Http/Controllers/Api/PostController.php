d743124b04cacb3e8366c0c921fc2bc9c8afbc03
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2022-02-23 22:08:08 +0100

    Prova json

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -7,5 +7,10 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index(){
+        return response()->json([
+            "nome" => "Andre",
+            "cognome" => "dada"
+        ]);
+    }
 }
